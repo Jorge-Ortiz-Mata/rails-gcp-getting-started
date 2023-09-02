6530bb7174512a62b526c4c7ccbe7350b94fe695
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-09-02 18:52:08 -0500

    chore: update the default name to ruby on rails with gcp - getting started

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "sales_app_production"
+  # config.active_job.queue_name_prefix = "ruby_on_rails_gcp_getting_started_production"
 
   config.action_mailer.perform_caching = false
 
